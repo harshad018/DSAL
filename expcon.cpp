beb2262753dcc2b3c1a9d3a1c38222025f0ed9ef
Harshad Ghutukade <108193335+harshad018@users.noreply.github.com>
2023-10-20 11:16:43 +0530

    new file of expcon

--- expcon.cpp
+++ expcon.cpp
@@ -1,5 +1,10 @@
 #include"stack.cpp"
 
+#include<iostream>
+#include<string.h>
+
+
+using namespace std;
 
 class expcon{
 
@@ -7,15 +12,23 @@ class expcon{
 char inf[20],pre[20], post[20];
 
 public:
+	expcon();
 	void inf2pre();
 	void inf2post();
 	void preeval();
 	void posteval();
 	void strreve(char a[]);
+	int icp( char x);
+	int isp( char x);
 
 };
 
-void strreve(char a[]){
+expcon::expcon(){
+ 
+
+ }
+
+void expcon::strreve(char a[]){
 
 	char temp;
 	int i = 0;
@@ -37,7 +50,76 @@ void strreve(char a[]){
 
 }
 
-void inf2pre(){
+int expcon::icp(char x){
+
+	int q;
+	
+	switch (x){
+	
+		case '+' : q = 1;
+			break;
+			
+		case '-': q = 1;
+			break;
+			
+		case '*': q =2;
+			break;
+			
+		case '/': q =2;
+			break;
+			
+		case '%': q=3;
+			break;
+			
+		case '^': q =4;
+			break;
+			
+		
+	
+	
+	
+	}
+
+	return q;
+
+}
+
+int expcon::isp(char x){
+
+	int q;
+	
+	switch (x){
+	
+		case '+' : q = 1;
+			break;
+			
+		case '-': q = 1;
+			break;
+			
+		case '*': q =2;
+			break;
+			
+		case '/': q =2;
+			break;
+			
+		case '%': q=3;
+			break;
+			
+		case '^': q =4;
+			break;
+			
+		
+	
+	
+	
+	
+	}
+
+	return q;
+
+}
+
+void expcon::inf2pre(){
 
 	int j=0;
 	stack s;
@@ -46,7 +128,7 @@ void inf2pre(){
 	
 	strreve(inf);
 	
-	for ( int i =0; i< strlen(a);i++){
+	for ( int i =0; i< strlen(inf);i++){
 	
 		if ( inf[i] == '('){
 		
@@ -54,13 +136,14 @@ void inf2pre(){
 		}
 		else if ( inf[i] == ')'){
 		
-			inf[i] = 'c'
+			inf[i] = '(';
 		}
-		
-		for ( int i = 0; i < strlen(a); i++)
+		}
+		cout<<inf<<endl;
+		for ( int i = 0; i < strlen(inf); i++)
 		{
 		
-			char x = inf[i];
+			int x = inf[i];
 			
 			if ( isalnum(x)){
 			
@@ -69,11 +152,78 @@ void inf2pre(){
 			else if ( x == '('){
 			
 				s.push(x);
+				cout<<"s"<<endl;
+			}
+			
+			else if ( x == ')'){
+			
+				while ( (x = s.pop()) != '('){
+					pre[j++] = x;
+				
+				}
+				
+			}
+			
+			else{
+			
+				while (!s.isempty() && icp(x) <= isp(s.topdata())){
+				
+				pre[j++] = s.pop();
+				
+				}
+			
+				s.push(x);
+			
+			}
+		
+		
+		}
+	
+		while ( !s.isempty()){
+		
+			pre[j++] = s.pop();
+		
+		}
+	
+		pre[j] = '\0';
+		strreve(pre);
+		
+		cout<<"prefix of the given expression is: "<<pre<<endl;
+	
+	}
+
+
+
+
+
+
+void expcon::inf2post(){
+
+int j=0;
+	stack s;
+	cout<<"Enter the infix expression"<<endl;
+	cin>>inf;
+	
+	
+	
+	
+		for ( int i = 0; inf[i] != '\0'; i++)
+		{
+		
+			char x = inf[i];
+			
+			if ( isalnum(x)){
+			
+				post[j++] = x;
+			}
+			else if ( x == '('){
+			
+				s.push(x);
 			}
 			else if ( x == ')'){
 			
 				while ( (x = s.pop()) != '('){
-					pre[j++} = x;
+					post[j++] = x;
 				
 				}
 			
@@ -81,26 +231,61 @@ void inf2pre(){
 			
 			else{
 			
+				while (!(s.isempty()) && icp(x) <= isp(s.topdata())){
+				
+				post[j++] = s.pop();
+				
+				}
 			
-			
-			
-			
-			
-			
+				s.push(x);
 			
 			}
 		
 		
+		}
+	
+		while ( !(s.isempty())){
+		
+			post[j++] = s.pop();
+		
+		}
+	
+		cout<<"postfix expression is: "<<post<<endl;
+	
+	}
+
+
+
+
+
+
+
+
+int main (){
+
+expcon e;
+		int ch;
+		
+	do {
+		
+		cout<<"Enter the operation that you want to perform: 1.infix to prefix 2.infix to postfix 3.exit"<<endl;
+		cin>>ch;
+	
+		switch (ch){
+		
+		
+			case 1: e.inf2pre();
+				break;
+				
+			case 2: e.inf2post();
+				break;
 		
 		
 		}
 	
 	
 	
-	
-	
-	}
-
+	}while (  ch < 3);
 
 
 
